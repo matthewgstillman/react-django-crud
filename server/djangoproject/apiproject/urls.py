057c236b4a985a382fbb7f61b4a35eb4be4102d4
matthewgstillman <matthewgstillman@gmail.com>
2024-10-01 18:35:51 -0600

    Add Create Book

--- server/djangoproject/apiproject/urls.py
+++ server/djangoproject/apiproject/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import get_books
+from .views import get_books, create_book
 
 urlpatterns = [
     path('books/', get_books, name='get_books'),
+    path('books/create', get_books, name='create_book'),
 ]
