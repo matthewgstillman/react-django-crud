19f2c06b2bde1198fccfb70c2f85ca80b5f81210
matthewgstillman <matthewgstillman@gmail.com>
2024-10-04 20:35:32 -0600

    book delete, update add

--- client/app/src/App.jsx
+++ client/app/src/App.jsx
@@ -53,6 +53,8 @@ function App() {
           <div>
             <p>Title: {book.title}</p>
             <p>Release Year: {book.release_year}</p>
+            <input type="text" placeholder="New title..." />
+            <button>Change Title</button>
           </div>
         )}
       </div>
