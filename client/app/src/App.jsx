61607303e588d407ba89696f04eb2ce7110a1807
matthewgstillman <matthewgstillman@gmail.com>
2024-10-07 15:09:11 -0600

    App.jsx update and delete add

--- client/app/src/App.jsx
+++ client/app/src/App.jsx
@@ -1,11 +1,13 @@
-import { useEffect, useState } from 'react'
-import './App.css'
+
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
   const [books, setBooks] = useState([]);
   const [title, setTitle] = useState("");
   const [releaseYear, setReleaseYear] = useState(0);
-  
+
+  const [newTitle, setNewTitle] = useState("");
 
   useEffect(() => {
     fetchBooks();
@@ -13,13 +15,13 @@ function App() {
 
   const fetchBooks = async () => {
     try {
-      const response = await fetch("http://127.0.0.1:8000/apiproject/books/");
+      const response = await fetch("http://127.0.0.1:8000/api/books/");
       const data = await response.json();
       setBooks(data);
-    } catch (error) {
-      console.log(error);
+    } catch (err) {
+      console.log(err);
     }
-  }
+  };
 
   const addBook = async () => {
     const bookData = {
@@ -42,24 +44,81 @@ function App() {
     }
   };
 
+  const updateTitle = async (pk, release_year) => {
+    const bookData = {
+      title: newTitle,
+      release_year,
+    };
+    try {
+      const response = await fetch(`http://127.0.0.1:8000/api/books/${pk}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(bookData),
+      });
+
+      const data = await response.json();
+      setBooks((prev) =>
+        prev.map((book) => {
+          if (book.id === pk) {
+            return data;
+          } else {
+            return book;
+          }
+        })
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const deleteBook = async (pk) => {
+    try {
+      const response = await fetch(`http://127.0.0.1:8000/api/books/${pk}`, {
+        method: "DELETE",
+      });
+
+      setBooks((prev) => prev.filter((book) => book.id !== pk));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <>
-      <h1>Book Website</h1>
+      <h1> Book Website </h1>
+
       <div>
-        <input type="text" placeholder="Book Title..." onChange={(e) => setTitle(event.target.value)}/>
-        <input type="number" placeholder="Release Date..." onChange={(e) => setReleaseYear(event.target.value)}/>
-        <button onClick={addBook}>Add Book</button>
-        {books.map((book) => 
-          <div>
-            <p>Title: {book.title}</p>
-            <p>Release Year: {book.release_year}</p>
-            <input type="text" placeholder="New title..." />
-            <button>Change Title</button>
-          </div>
-        )}
+        <input
+          type="text"
+          placeholder="Book Title..."
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Release Year..."
+          onChange={(e) => setReleaseYear(e.target.value)}
+        />
+        <button onClick={addBook}> Add Book </button>
       </div>
+      {books.map((book) => (
+        <div>
+          <p>Title: {book.title}</p>
+          <p>Release Year: {book.release_year} </p>
+          <input
+            type="text"
+            placeholder="New Title..."
+            onChange={(e) => setNewTitle(e.target.value)}
+          />
+          <button onClick={() => updateTitle(book.id, book.release_year)}>
+            Change Title
+          </button>
+          <button onClick={() => deleteBook(book.id)}> Delete</button>
+        </div>
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
